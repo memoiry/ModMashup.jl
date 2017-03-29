df4b995796fce8d939b22ef2421226b4fa3f0ea7
memoiry <memoiry@outlook.com>
2017-03-29 21:54:46 +0800

    test

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,19 @@
-push!(LOAD_PATH, "../src")
-
 using Documenter, GeneMANIA
 
 
-makedocs()
+makedocs(
+    format = :html,
+    sitename = "GeneMANIA",
+    pages = Any[
+        "Home" => "index.md"
+        ]
+
+    )
 
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/memoiry/GeneMANIA.jl.git",
-    julia  = "0.5",
-    osname = "osx"
+    target = "build",
+    deps = nothing,
+    deps = nothing,
 )

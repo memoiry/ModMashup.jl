1543b45fdc1adbfb606cf25728a6a8cf85d7b252
memoiry <memoiry@outlook.com>
2017-05-06 21:24:54 +0800

    theme test

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,24 @@
 using Documenter, GeneMANIA
 
 
-makedocs(modules=[GeneMANIA],
-        doctest=true)
+makedocs(clean = false,
+        format = :html,
+        sitename = "GeneMANIA.jl",
+        pages = Any[
+        "Home" => "index.md",
+        "Quick Start" => "dev/get_start.md",
+        "Algorithms" => Any[
+        "Label Propagation" => "algo/label_propagation.md",
+        "Network Integration" => "algo/network_integration.md"
+        ]
+    ]
+)
 
 
 deploydocs(
     repo = "github.com/memoiry/GeneMANIA.jl.git",
     julia = "0.5",
+    target = "build",
+    deps = nothing,
+    make = nothing,
 )

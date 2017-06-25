5b91f690d9869cb6e4a1a8554eec92d1a4400ab9
memoiry <memoiry@outlook.com>
2017-06-26 01:33:28 +0800

    making test stable

--- test/integration_alg_test.jl
+++ test/integration_alg_test.jl
@@ -4,10 +4,7 @@ using GeneMANIA
 function test()
     dir = "ex/KIRC/data/networks"
     disease_file = "ex/KIRC/data/annotations/disease.csv"
-    index_file = "ex/KIRC/disease_index.txt"
-    net_sel = ["age","grade","stage"]
-
-    database = GMANIA(dir,disease_file,index_file = index_file,net_sel = net_sel)
+    database = GMANIA(dir,disease_file, query_attr = 1)
     model = MashupIntegration()
     network_integration!(model, database)
     true
